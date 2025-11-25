089f8a53d6d2d3c03241994d9d1d64015af6038d
Andreas Gabel <andreassgabel@hotmail.com>
2025-11-25 18:23:30 +0100

    First commit

---
+++ src/main/java/org/ek/portofoliobackend/PortofolioBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ek.portofoliobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortofolioBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortofolioBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ek/portofoliobackend/PortofolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ek.portofoliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortofolioBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
