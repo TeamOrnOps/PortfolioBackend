9b1f99216450a0751681322f3f43609991cd127a
Nima <nimaginobilisalami@gmail.com>
2025-12-01 15:24:25 +0100

    ImageResponse DTO med relevante felter

--- src/main/java/org/ek/portfoliobackend/dto/request/UpdateProjectRequest.java
+++ src/main/java/org/ek/portfoliobackend/dto/request/UpdateProjectRequest.java
@@ -1,32 +1,26 @@
 package org.ek.portfoliobackend.dto.request;
 
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.antlr.v4.runtime.misc.NotNull;
 
+import org.ek.portfoliobackend.model.ServiceCategory;
+import org.ek.portfoliobackend.model.CustomerType;
 import java.time.LocalDate;
+
 
 @Data // indeholder getter/setter + toString(), equals(), hashCode() (Sidste to er måske ikke nødvendige, men de er der)
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProjectRequest {
+public class UpdateProjectRequest {
 
-    @NotNull(message = "Projektet skal have en title.")
-    @NotBlank
+
     private String title;
-
-    @NotNull(message = "Der skal være en beskrivelse til projektet.")
     private String description;
-
-    // ENUMS
-    private ServiceCategory serviceCategory; // Kan vi ændre klassen til at hedde JobTypeCategory i stedet?
-    private Customertype costumerType;
-
-
+    private ServiceCategory serviceCategory; // TODO: Kan vi ændre klassen til at hedde JobTypeCategory i stedet?
+    private CustomerType customerType;
     private LocalDate executionDate;
-
-
 
 }
