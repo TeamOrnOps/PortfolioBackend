855b5f3f4f956a5e9ee385bc4c92c330d688a881
Sofie Broman <sofierytter@hotmail.com>
2025-12-08 13:48:13 +0100

    UserDetailsService implementeret med hardcoded admin-rolle

---
+++ src/main/java/org/ek/portfoliobackend/service/UserDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package org.ek.portfoliobackend.service;
+
+import org.ek.portfoliobackend.model.User;
+import org.ek.portfoliobackend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static org.springframework.security.core.userdetails.User.withUsername;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final UserDetailsPasswordService userDetailsPasswordService;
+
+    public UserDetailsServiceImpl(UserRepository userRepository, UserDetailsPasswordService userDetailsPasswordService) {
+        this.userRepository = userRepository;
+        this.userDetailsPasswordService = userDetailsPasswordService;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles("ADMIN")   // All users = ADMIN
+                .build();
+    }
+
+
+    }
+
