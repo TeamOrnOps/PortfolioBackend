fc4f43702ddbf0dcc86fccd90fd02f99703d3fe7
Sofie Broman <sofierytter@hotmail.com>
2025-12-08 15:31:03 +0100

    JWT token generator fkommenteret

--- src/main/java/org/ek/portfoliobackend/security/filter/JwtTokenGenerator.java
+++ src/main/java/org/ek/portfoliobackend/security/filter/JwtTokenGenerator.java
@@ -16,35 +16,42 @@ import java.util.Date;
 @Component
 public class JwtTokenGenerator {
 
-    @Value("${JWT_SECRET}")
+    @Value("${JWT_SECRET}") // Used for the server to sign and validate tokens
     private String secret;
 
 
-    private Key getSigningKey() {
+    private Key getSigningKey() { // Converts the secret String into a secure HMAC-SHA-256 signing key
         return Keys.hmacShaKeyFor(secret.getBytes()); // Secret must be of 32 chars for HS256
     }
 
+    // Token generator (creates the claims of a user)
     public String generateToken(UserDetails user) {
         return Jwts.builder()
-                .claim("roles", user.getAuthorities())
+                .claim("roles", user.getAuthorities()) // Stores the user's authorities (ADMIN) inside the token
                 .setSubject(user.getUsername())
+                .setIssuedAt(new Date()) // Token generation date
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // Token expires after 24 hours
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256) // JJWT signing
-                .compact();
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256) // Signs the token with the HMAC-SHA256 algorithm
+                .compact(); // Converts the token to a String for frontend
 
     }
 
+
+    // Reads the token
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder() // Parsing the token
+                .setSigningKey(getSigningKey()) // Validates the signature
+                .build()
+                .parseClaimsJws(token) // Parses and validates the token
+                .getBody();
+
+    }
+
+    // Gets the subject (user)
     public String extractUsername(String token) {
         return extractAllClaims(token).getSubject();
     }
 
-    public Claims extractAllClaims(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getSigningKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-
-    }
+    // Claim = subject (user), expiration time, issued at time and role
 
 }
